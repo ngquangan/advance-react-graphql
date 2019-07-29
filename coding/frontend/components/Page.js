976a4b25d97784814b542b429ac9a77bccd86203
Nguyen Quang An <ngquangan@gmail.com>
2019-07-29 19:58:10 +0700

    Create home page with meta, header and custom app

---
+++ coding/frontend/components/Page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header';
+import Meta from './Meta';
+
+const Page = (props) => {
+  return (
+    <div>
+      <Meta />
+      <Header />
+      { props.children }
+    </div>
+  )
+}
+
+export default Page
---
+++ coding/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+const Sell = () => {
+  return (
+    <div>
+      sell
+    </div>
+  )
+};
+
+export default Sell;
