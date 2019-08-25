e222de3b403db31985f083a45e6d630617b9d89e
An Nguyen Quang <ngquangan@gmail.com>
2019-08-25 16:18:22 +0700

    Add prisma and connect to server

--- coding/backend/src/resolvers/Mutation.js
+++ coding/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const mutations = {
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+                data: {
+                    ...args,
+                },
+            },
+            info
+        );
+        console.log(item);
+        return item;
+    },
+};
 
 module.exports = mutations;
